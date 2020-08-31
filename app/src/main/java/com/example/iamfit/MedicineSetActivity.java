c2abbeda5896144bd7dff4c5ec8e228b91d0aede
2018331043 <mahnur43@student.sust.edu>
2020-08-31 23:02:41 +0600

    Custom notifications for multiple medicines at multiple times functionality done

--- app/src/main/java/com/example/iamfit/MedicineSetActivity.java
+++ app/src/main/java/com/example/iamfit/MedicineSetActivity.java
@@ -28,7 +28,7 @@ import java.util.Locale;
 public class MedicineSetActivity extends AppCompatActivity {
     public EditText medicineName,timeH,timeM,daysToLast;
     public static String MEDICINE_NAME="com.example.iamfit.MedicineName";
-    public ArrayList<Medicine> mediciness=new ArrayList<Medicine>();
+    private ArrayList<Medicine> mediciness=new ArrayList<Medicine>();
     public Button setReminder;
     public SimpleDateFormat sfH,sfM,sfS;
     @Override
@@ -40,95 +40,108 @@ public class MedicineSetActivity extends AppCompatActivity {
         timeM=findViewById(R.id.dosetime2);
         daysToLast=findViewById(R.id.dosetime3);
         setReminder=findViewById(R.id.button3);
+        SharedPreferences sharedPreferences =getSharedPreferences("MedicineList",MODE_PRIVATE);
+        SharedPreferences.Editor editor=sharedPreferences.edit();
+        Gson gson=new Gson();
+        String json=gson.toJson(mediciness);
+        editor.putString("ListOfTheMedicines",json);
+        editor.apply();
         setReminder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String medName=medicineName.getText().toString();
-                String hour=timeH.getText().toString();
-                String minute=timeM.getText().toString();
-                String days=daysToLast.getText().toString();
-                int length=days.length();
-                int daysrem=0;
-                int temp=0;
-                for(int i=length-1;i>=0;i--){
-                    daysrem+=(days.toCharArray()[i]-48)*Math.pow(10,temp);
+                String medName = medicineName.getText().toString();
+                String hour = timeH.getText().toString();
+                String minute = timeM.getText().toString();
+                String days = daysToLast.getText().toString();
+                SharedPreferences sharedPreferences =getSharedPreferences("MedicineIterator",MODE_PRIVATE);
+                SharedPreferences.Editor editor=sharedPreferences.edit();
+                editor.putInt("Iterator",0);
+                editor.apply();
+                int length = days.length();
+                int daysrem = 0;
+                int temp = 0;
+                for (int i = length - 1; i >= 0; i--) {
+                    daysrem += (days.toCharArray()[i] - 48) * Math.pow(10, temp);
                     temp++;
                 }
-                temp=0;
-                Intent intent=new Intent(MedicineSetActivity.this,ReminderBroadcast.class);
-                PendingIntent pendingIntent= PendingIntent
-                        .getBroadcast(MedicineSetActivity.this,0,intent,0);
-                 ArrayList <AlarmManager> alarmManagers=new ArrayList<>();
-                AlarmManager alarmManager= (AlarmManager)getSystemService(ALARM_SERVICE);
-                Date calendar= Calendar.getInstance().getTime();
+                temp = 0;
+                Intent intent = new Intent(MedicineSetActivity.this, ReminderBroadcast.class);
 
-                sfH=new SimpleDateFormat("HH");
-                sfM=new SimpleDateFormat("mm");
-                sfS=new SimpleDateFormat("ss");
-                String curH,curM,curS;
-                curH=sfH.format(calendar);
-                curM=sfM.format(calendar);
-                curS=sfS.format(calendar);
-                int cH,cM,cS,reqH,reqM,reqS,dif=0;
-                cH=(curH.toCharArray()[1]-48);
-                cH+=(curH.toCharArray()[0]-48)*10;
-                cM=curM.toCharArray()[1]-48;
-                cM+=(curM.toCharArray()[0]-48)*10;
-                cS=curS.toCharArray()[1]-48;
-                cS+=(curS.toCharArray()[0]-48)*10;
-                reqH=(hour.toCharArray()[1]-48);
-                reqH+=10*(hour.toCharArray()[0]-48);
-                reqM=minute.toCharArray()[1]-48;
-                reqM+=10*(minute.toCharArray()[0]-48);
-                if(reqH-cH<0){
-                    reqH+=24;
-                    dif+=(reqH-cH)*60;
-                }else{
-                    dif+=(reqH-cH)*60;
+                ArrayList<AlarmManager> alarmManagers = new ArrayList<>();
+                //AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+                Date calendar = Calendar.getInstance().getTime();
+
+                sfH = new SimpleDateFormat("HH");
+                sfM = new SimpleDateFormat("mm");
+                sfS = new SimpleDateFormat("ss");
+                String curH, curM, curS;
+                curH = sfH.format(calendar);
+                curM = sfM.format(calendar);
+                curS = sfS.format(calendar);
+                int cH, cM, cS, reqH, reqM, reqS, dif = 0;
+                cH = (curH.toCharArray()[1] - 48);
+                cH += (curH.toCharArray()[0] - 48) * 10;
+                cM = curM.toCharArray()[1] - 48;
+                cM += (curM.toCharArray()[0] - 48) * 10;
+                cS = curS.toCharArray()[1] - 48;
+                cS += (curS.toCharArray()[0] - 48) * 10;
+                reqH = (hour.toCharArray()[1] - 48);
+                reqH += 10 * (hour.toCharArray()[0] - 48);
+                reqM = minute.toCharArray()[1] - 48;
+                reqM += 10 * (minute.toCharArray()[0] - 48);
+                if (reqH - cH < 0) {
+                    reqH += 24;
+                    dif += (reqH - cH) * 60;
+                } else {
+                    dif += (reqH - cH) * 60;
                 }
-                    dif+=(reqM-cM);
-
-
+                dif += (reqM - cM);
                 loadMedicineData();
-                Medicine medicine =new Medicine(dif,reqH,reqM,daysrem,medName);
+                Medicine medicine = new Medicine(dif, reqH, reqM, daysrem, medName);
                 mediciness.add(medicine);
-                temp=1;
-                while(temp==1){
-                    temp=0;
-                    for(int i=0;i<mediciness.size()-1;i++){
-                        if(mediciness.get(i).getDif()>mediciness.get(i+1).getDif()){
-                            temp=1;
-                            Medicine te= mediciness.get(i);
-                            mediciness.set(i,mediciness.get(i+1));
-                            mediciness.set(i+1,te);
+                temp = 1;
+                while (temp == 1) {
+                    temp = 0;
+                    for (int i = 0; i < mediciness.size() - 1; i++) {
+                        if (mediciness.get(i).getDif() > mediciness.get(i + 1).getDif()) {
+                            temp = 1;
+                            Medicine te = mediciness.get(i);
+                            mediciness.set(i, mediciness.get(i + 1));
+                            mediciness.set(i + 1, te);
                         }
                     }
                 }
                 saveMedicineData();
-                //ReminderBroadcast.Medicines.add(medicine);
-
-                for(int i=0;i<mediciness.size();i++){
-                    Toast.makeText(MedicineSetActivity.this, mediciness.get(i).getName(), Toast.LENGTH_LONG).show();
+                for (int i = 0; i < mediciness.size(); i++) {
+                    Toast.makeText(MedicineSetActivity.this, "found "+mediciness.get(i).getDif(), Toast.LENGTH_LONG).show();
                 }
-                long now=System.currentTimeMillis()-cS*1000;
-                Toast.makeText(MedicineSetActivity.this, "Hi"+dif, Toast.LENGTH_LONG).show();
-                //if(mediciness.size()==1){
-                    Toast.makeText(MedicineSetActivity.this, "Triggered 1", Toast.LENGTH_LONG).show();
-                    alarmManager.set(AlarmManager.RTC_WAKEUP,
-                            now+(dif*60*1000),
-                            pendingIntent);
-                //}
-                //else{
-                  //  Toast.makeText(MedicineSetActivity.this, "Triggered 2", Toast.LENGTH_LONG).show();
-                    //alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,now+mediciness.get(0).getDif()
-                        //    ,
-                      //      ,pendingIntent);
-                //}
+                long now = System.currentTimeMillis() - cS * 1000;
+               // Toast.makeText(MedicineSetActivity.this, "Hi" + dif, Toast.LENGTH_LONG).show();
+                alarmManagers.clear();
+                for (int i = 0; i < mediciness.size(); i++) {
+                    //Toast.makeText(MedicineSetActivity.this, "Hi" + i+1, Toast.LENGTH_LONG).show();
+                    if ( mediciness.get(i).getHour() - cH < 0) {
+                        //reqH += 24;
+                       mediciness.get(i).setDif((mediciness.get(i).getHour()+24 - cH) * 60);
+                    } else {
+                        mediciness.get(i).setDif((mediciness.get(i).getHour() - cH) * 60);
+                    }
+                    mediciness.get(i).setDif(mediciness.get(i).getDif()+(mediciness.get(i).getMinute() - cM));
 
+                    PendingIntent pendingIntent = PendingIntent
+                            .getBroadcast(MedicineSetActivity.this, i, intent, 0);
+                    AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+                    if(mediciness.get(i).getDif()<0)continue;
+                    Toast.makeText(MedicineSetActivity.this, "Hi " +i+" "+mediciness.get(i).getDif(), Toast.LENGTH_LONG).show();
+                    alarmManagers.add(alarmManager);
+                    alarmManagers.get(i).set(AlarmManager.RTC_WAKEUP,now+mediciness.get(i).getDif()*60*1000
+                            ,pendingIntent);
+                }
             }
         });
     }
-    public void loadMedicineData(){
+    private void loadMedicineData(){
+        mediciness.clear();
         SharedPreferences sharedPreferences =getSharedPreferences("MedicineList",MODE_PRIVATE);
         Gson gson=new Gson();
         String json=sharedPreferences.getString("ListOfTheMedicines",null);
@@ -138,7 +151,7 @@ public class MedicineSetActivity extends AppCompatActivity {
             mediciness=new ArrayList<>();
         }
     }
-    public void saveMedicineData(){
+    private void saveMedicineData(){
         SharedPreferences sharedPreferences =getSharedPreferences("MedicineList",MODE_PRIVATE);
         SharedPreferences.Editor editor=sharedPreferences.edit();
         Gson gson=new Gson();
--- app/src/main/java/com/example/iamfit/ReminderBroadcast.java
+++ app/src/main/java/com/example/iamfit/ReminderBroadcast.java
@@ -4,26 +4,42 @@ import android.app.Notification;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.widget.Toast;
 
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+
+import static android.content.Context.MODE_PRIVATE;
 
 public class ReminderBroadcast extends BroadcastReceiver {
     private NotificationManagerCompat notificationManagerCompat;
     //public static ArrayList<String> MedicineNames=new ArrayList<>();
-    public static ArrayList<Medicine> Medicines=new ArrayList<Medicine>();
-    public static Integer medicineNumber=0,alarmManagerNumbers=0;
+    private ArrayList<Medicine> mediciness=new ArrayList<Medicine>();
+   // public static ArrayList<Medicine> Medicines=new ArrayList<Medicine>();
+   // public static Integer medicineNumber=0,alarmManagerNumbers=0;
     @Override
     public void onReceive(Context context, Intent intent) {
 
         notificationManagerCompat=NotificationManagerCompat.from(context);
         //final String MedicineName=intent.getStringExtra(MedicineSetActivity.MedicineNames.get(MedicineSetActivity.medicineNumber));
-        String MedicineName=Medicines.get(medicineNumber).getName();
+       // String MedicineName=Medicines.get(medicineNumber).getName();
+        int it=loadAndRetrieveIterator(context);
 
-        Toast.makeText(context,"Hello : ", Toast.LENGTH_SHORT).show();
+       // =sharedPreferences.getInt("Iterator",0);
+       //int it=0;
+        loadMedicineData(context);
+        String MedicineName=mediciness.get(it).getName();
+        //it++;
+       // editor.putInt("Iterator",it);
+       // editor.commit();
+        Toast.makeText(context,"Hello : "+it, Toast.LENGTH_SHORT).show();
         Notification notification=new NotificationCompat.Builder(context,App.MEDICINE_TAKE)
                 .setSmallIcon(R.drawable.heart)
                 .setContentTitle(MedicineName)
@@ -31,7 +47,27 @@ public class ReminderBroadcast extends BroadcastReceiver {
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setCategory(NotificationCompat.CATEGORY_MESSAGE)
                 .build();
-        notificationManagerCompat.notify(medicineNumber+1,notification);
-        medicineNumber++;
+        notificationManagerCompat.notify(it+1,notification);
+       // medicineNumber++;
+    }
+    public void loadMedicineData(Context context){
+        mediciness.clear();
+        SharedPreferences sharedPreferences =context.getSharedPreferences("MedicineList",MODE_PRIVATE);
+        Gson gson=new Gson();
+        String json=sharedPreferences.getString("ListOfTheMedicines",null);
+        Type type=new TypeToken<ArrayList<Medicine>>() {}.getType();
+        mediciness=gson.fromJson(json,type);
+        if(mediciness==null){
+            mediciness=new ArrayList<>();
+        }
+    }
+    private int loadAndRetrieveIterator(Context context){
+        SharedPreferences sharedPreferences =context.getSharedPreferences("MedicineIterator",MODE_PRIVATE);
+        SharedPreferences.Editor editor=sharedPreferences.edit();
+        int te=sharedPreferences.getInt("Iterator",0);
+        te++;
+        editor.putInt("Iterator",te);
+        editor.apply();
+        return (te-1);
     }
 }
