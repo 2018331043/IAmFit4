14676d0df1645a9c2e6317b141127418f65fe705
2018331043 <mahnur43@student.sust.edu>
2020-12-19 19:40:42 +0600

    Image Uploading in firebase done image loading pending.

--- app/src/main/java/com/example/iamfit/SettingsActivity.java
+++ app/src/main/java/com/example/iamfit/SettingsActivity.java
@@ -1,23 +1,42 @@
 package com.example.iamfit;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.net.URI;
+import java.util.UUID;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -25,6 +44,11 @@ public class SettingsActivity extends AppCompatActivity {
     TextView displayName;
     DatabaseReference databaseReference;
     Button editDetailsButton;
+    private Button upoladPic;
+    private StorageReference storageReference;
+    private Uri imageUri;
+    private ImageView profilePic;
+    private FirebaseStorage firebaseStorage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,7 +56,18 @@ public class SettingsActivity extends AppCompatActivity {
         logoutButton=findViewById(R.id.imageButtonLogOut);
         displayName=findViewById(R.id.textViewSettingsDisplayName);
         editDetailsButton=findViewById(R.id.button4);
+        profilePic=findViewById(R.id.profile_image_recyclerview_child);
         databaseReference= FirebaseDatabase.getInstance().getReference();
+        firebaseStorage=FirebaseStorage.getInstance();
+        //imageUri=new Uri();
+        storageReference=FirebaseStorage.getInstance().getReference();
+        upoladPic=findViewById(R.id.button3);
+        upoladPic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                upLoadPhoto();
+            }
+        });
         databaseReference.child("Users").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -66,5 +101,72 @@ public class SettingsActivity extends AppCompatActivity {
             }
         });
 
+    }
+    void upLoadPhoto() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 1);
+        /*if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
+            /*ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},REQUEST_CODE2);
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.INTERNET},REQUEST_CODE3);
+        //return;
+        }*/
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==1&& resultCode==RESULT_OK && data!=null &&data.getData()!=null){
+            imageUri=data.getData();
+            //profilePic.setImageURI(imageUri);
+            uploadPicToFirebase();
+        }
+    }
+
+    private void uploadPicToFirebase() {
+        //Uri file = Uri.fromFile(new File("path/to/images/rivers.jpg"));
+        final ProgressDialog pd =new ProgressDialog(this);
+        pd.setTitle("Uploading Image...");
+        pd.show();
+
+        final String UID=FirebaseAuth.getInstance().getCurrentUser().getUid().toString();
+        //final String UID= UUID.randomUUID().toString();
+        StorageReference riversRef = storageReference.child("images/"+UID+".jpg");
+        profilePic.setImageURI(imageUri);
+        //Toast.makeText(SettingsActivity.this, "Upload Unsuccessful", Toast.LENGTH_LONG).show();
+        //if(riversRef==null){
+            //Toast.makeText(SettingsActivity.this, "Upload Unsuccessful"+riversRef, Toast.LENGTH_LONG).show();
+        //}
+        riversRef.putFile(imageUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        pd.dismiss();
+                        // Get a URL to the uploaded content
+                        //Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                        //Snackbar.make(findViewById(R.id.content),"Image Uploaded",Snackbar.LENGTH_LONG).show();
+                        //Toast.makeText(SettingsActivity.this, "Upload Unsuccessful", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        pd.dismiss();
+                        Toast.makeText(SettingsActivity.this, "Upload Unsuccessful", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                        double progressPercent =(100.00*taskSnapshot.getBytesTransferred()/taskSnapshot.getTotalByteCount());
+                        pd.setMessage("Progress: "+(int)progressPercent+"%");
+                    }
+                });
     }
 }
