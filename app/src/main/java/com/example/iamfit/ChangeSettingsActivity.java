968b28a4cb6b3c13e470840701919b976d0cd09b
2018331043 <mahnur43@student.sust.edu>
2021-02-22 01:04:46 +0600

    Bmi show bug after every steps solve

--- app/src/main/java/com/example/iamfit/ChangeSettingsActivity.java
+++ app/src/main/java/com/example/iamfit/ChangeSettingsActivity.java
@@ -3,12 +3,42 @@ package com.example.iamfit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ChangeSettingsActivity extends AppCompatActivity {
+    private TextView name,heightF,heightI,weight;
+    private Button doneButton;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_settings);
+        name=findViewById(R.id.editTextTextPersonName4);
+        heightF=findViewById(R.id.editTextTextPersonName5);
+        heightI=findViewById(R.id.editTextNumber);
+        weight=findViewById(R.id.editTextNumber2);
+        doneButton=findViewById(R.id.button5);
+        databaseReference= FirebaseDatabase.getInstance().getReference("Users");
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!name.getText().toString().isEmpty()){
+                    databaseReference.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("name").setValue(name.getText().toString());
+                }
+                if(!heightF.getText().toString().isEmpty()){
+                    databaseReference.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("height").setValue(heightF+"."+heightI);
+                }
+                if(!weight.getText().toString().isEmpty()){
+                    databaseReference.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("weight").setValue(weight.getText().toString());
+                }
+            }
+        });
     }
 }
